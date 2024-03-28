1cd0d3468b90fcc525921136b4d2fb44e13d6fd3
Ricardo Romo <jricardoromo21@gmail.com>
2024-03-28 17:45:58 -0500

    fix vercel library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
